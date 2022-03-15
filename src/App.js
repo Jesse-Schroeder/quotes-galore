1875767e86fa7f275d4a68e439cb92a46cb99cb3
Jesse-Schroeder <94391760+Jesse-Schroeder@users.noreply.github.com>
2022-03-15 11:32:47 -0400

    Personalized the App function

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Quotes-Galore">
+      <header className="Click-Here-For-Wisdom">
+        <img src={logo} className="Quotes-Galore-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="Quotes-Galore-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
